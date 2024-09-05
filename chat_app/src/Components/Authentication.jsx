defcc09bbaf548041f761999657b6a5e346f3340
Dharma41 <dharmendrareddy2000@gmail.com>
2024-09-05 18:55:08 -0500

    AUTHENTICATION FORM IS CREATED ALONG WITH CSS FILE

--- chat_app/src/Components/Authentication.jsx
+++ chat_app/src/Components/Authentication.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import '../CSS/Authentication.css'
 
-const Authentication=()=>{
-    return(
-        <>
-            <h1>Hi from Authentication</h1>
-        </>
-    )
-}
+const Authentication = () => {
+  return (
+    <div className="auth-container">
+      <div className="auth-form">
+        <label htmlFor="username">Username:</label>
+        <input type="email" id="username" placeholder="Enter your email" />
+        
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" placeholder="Enter your password" />
+        
+        <button className="auth-button">Login</button>
+      </div>
+    </div>
+  );
+};
 
 export default Authentication;
