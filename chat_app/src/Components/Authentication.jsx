cecbb2dedd1c420d675a376b505b5a5a63654f82
Dharma41 <dharmendrareddy2000@gmail.com>
2024-09-05 18:35:40 -0500

    Authentication Component created

---
+++ chat_app/src/Components/Authentication.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Authentication=()=>{
+    return(
+        <>
+            <h1>Hi from Authentication</h1>
+        </>
+    )
+}
+
+export default Authentication;
